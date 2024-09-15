e9eae14e13769b3d2922cda09985afa6f5950f46
Tri <anhtri54@gmail.com>
2024-09-16 00:54:53 +0200

    yamazon

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,6 @@
 const products = [{
   image: `images/products/athletic-cotton-socks-6-pairs.jpg`,
-  name: ` Black and Gray Athletic Cotton Socks - 6 Pairs`,
+  name: ` Pistol Glock 19 Gen4 MOS 9x19`,
   rating: {
     stars: 4.5,
     count: 87
@@ -9,21 +9,21 @@ const products = [{
   priceCents: 1090,
 }, {
   image: `images/products/intermediate-composite-basketball.jpg`,
-  name: ` Intermediate Size Basketball`,
+  name: ` Hentai pillow *MINT CONDITION*`,
   rating: {
     stars: 4.0,
     count: 127
   },
-  priceCents: 2095
+  priceCents: 20069
 
 }, {
   image: `images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg`,
-  name: ` Adults Plain Cotton T-Shirt - 2 Pack`,
+  name: ` Lund University backpack *RARE CONDITION!!*`,
   rating: {
     stars: 4.5,
     count: 56
   },
-  priceCents: 799
+  priceCents: 6999
 
 }]
 
